4c7d01a76dc85eebb00a20d84e2b5d3effc4deb6
Sivatharan <sivatharanoff97@gmail.com>
2026-01-26 10:22:38 +0530

    divide places list as separate widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:favorite_places/screens/places_list.dart';
+import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
       title: 'Great Places',
       theme: theme,
       debugShowCheckedModeBanner: false,
-      home: const PlacesScreenList(),
+      home: const PlacesScreen(),
     );
   }
 }
---
+++ lib/widgets/places_list.dart
@@ -0,0 +1,45 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/place_details.dart';
+import 'package:flutter/material.dart';
+
+class PlacesList extends StatelessWidget {
+  const PlacesList({super.key, required this.places});
+
+  final List<Place> places;
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return Center(
+        child: Container(
+          padding: const EdgeInsets.only(bottom: 50),
+          child: Text(
+            'No places added yet',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ),
+      );
+    }
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          key: ValueKey(places[index].id),
+          title: Text(
+            places[index].tilte,
+          ),
+          onTap: () => {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => PlaceDetailsScreen(
+                  place: places[index],
+                ),
+              ),
+            ),
+          },
+        );
+      },
+      itemCount: places.length,
+    );
+  }
+}
