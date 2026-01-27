8ecdc3d2d231756c2afa5a54949fa2aebb821505
Sivatharan <sivatharanoff97@gmail.com>
2026-01-27 13:22:11 +0530

    add initial file for location input configure setting

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() {
+    return _LocationInput();
+  }
+}
+
+class _LocationInput extends State<LocationInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
