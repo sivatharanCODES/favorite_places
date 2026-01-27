7aebc830615c467b5b3ac142098d150786863c45
Sivatharan <sivatharanoff97@gmail.com>
2026-01-27 22:32:09 +0530

    add locatio details in place details page

--- lib/screens/place_details.dart
+++ lib/screens/place_details.dart
@@ -5,6 +5,24 @@ class PlaceDetailsScreen extends StatelessWidget {
   const PlaceDetailsScreen({super.key, required this.place});
 
   final Place place;
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+    final uri = Uri.https(
+      'maps.geoapify.com',
+      '/v1/staticmap',
+      {
+        'style': 'osm-bright-smooth',
+        'width': '600',
+        'height': '300',
+        'center': 'lonlat:$lng,$lat',
+        'zoom': '16',
+        'marker': 'lonlat:$lng,$lat;type:awesome;color:#d90000;size:x-large',
+        'apiKey': '1e6f6e759ae04c1d8ab596c93bc67a0a',
+      },
+    );
+    return uri.toString();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +38,43 @@ class PlaceDetailsScreen extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 26,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black54,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
